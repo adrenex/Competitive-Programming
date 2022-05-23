93a9b7826fd6912834c45c3de559b451ac8ed9ed
Nilesh Popli <77211167+adrenex@users.noreply.github.com>
2022-05-24 01:29:18 +0530

    Create SwapNodesInPairs.cpp

---
+++ SwapNodesInPairs.cpp
@@ -0,0 +1,27 @@
+/* Leetcode 24
+	 Swap Nodes in Pairs
+
+    Given a linked list, swap every two adjacent nodes and return its head. 
+    You must solve the problem without modifying the values in the list's nodes. */
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode *a, *b, *p;
+
+        if (head && head->next) {
+            a = head;
+            head = b = a->next;
+            a->next = b->next;
+            p = b->next = a;
+        }
+        while (p && p->next && p->next->next) {
+            a = p->next;
+            p->next = b = a->next;
+            a->next = b->next;
+            p = b->next = a;
+        }
+
+        return head;
+    }
+};
